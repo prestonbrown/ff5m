79b3f7643bfbdfe4b3850924a0883b0681475daf
Alexander <DrA1ex@users.noreply.github.com>
2025-10-01 19:05:46 +0500

    Update addMD5.bat (#47)

--- addMD5.bat
+++ addMD5.bat
@@ -1,7 +1,40 @@
 @echo off
-set prefecs=; MD5:
-FOR /F %%i IN ('certutil -hashfile %1 md5^|find /v "MD5"^|find /v "CertUtil"') DO set md5=%%i
-set payload=%prefecs%%md5%
-REM echo %payload%>>%1 
-SET filename=%1
-echo %payload% > %TEMP%\temp.md5 & type %filename:/=\% >> %TEMP%\temp.md5 & move /y %TEMP%\temp.md5 %filename:/=\% >nul
+setlocal enabledelayedexpansion
+
+REM Check if file parameter was provided
+if "%~1"=="" (
+echo Error: No file specified
+pause
+exit /b 1
+)
+
+REM Set the input file
+set "inputfile=%~1"
+
+REM Generate MD5 hash
+for /f "skip=1 delims=" %%i in ('certutil -hashfile "%inputfile%" md5') do (
+set "hash=%%i"
+goto :got_hash
+)
+
+:got_hash
+REM Remove spaces from hash
+set "hash=!hash: =!"
+
+REM Create the MD5 comment line
+set "md5_line=; MD5:!hash!"
+
+REM Create temporary file with MD5 header and original content
+echo !md5_line! > "%TEMP%\gcode_temp.txt"
+type "%inputfile%" >> "%TEMP%\gcode_temp.txt"
+
+REM Replace original file with new content
+move "%TEMP%\gcode_temp.txt" "%inputfile%" >nul 2>&1
+
+if errorlevel 1 (
+echo Error: Could not update file
+pause
+exit /b 1
+)
+
+echo MD5 hash added successfully
